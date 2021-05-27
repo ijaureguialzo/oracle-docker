271d1bd1c45fa3275818d716d1d9ceb71b2f295c
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2021-05-27 13:01:53 +0200

    Script mejorado

--- crear_usuario.sql
+++ crear_usuario.sql
@@ -1,11 +1,17 @@
 -- REF: http://www.dba-oracle.com/t_ora_65096_create_user_12c_without_c_prefix.htm
-alter session set "_ORACLE_SCRIPT"=true;
+alter session set "_ORACLE_SCRIPT" = true;
 
-drop user egibide cascade;
+begin
+    execute immediate 'drop user egibide cascade';
+exception
+    when others then
+        dbms_output.put_line('El usuario no existe.');
+end;
+/
 
 -- REF: http://oracle-apprentice.blogspot.com/2011/03/oracle-password-restriction-solution.html
 create user egibide
-identified by "12345Abcde";
+    identified by "12345Abcde";
 
 grant connect, resource to egibide;
 grant unlimited tablespace to egibide;
